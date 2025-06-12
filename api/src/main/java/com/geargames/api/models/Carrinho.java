7d62c2f3e71f2bf46dd299200fb450f745b85caf
Leonardosluna <leonardoluna26@gmail.com>
2025-06-11 21:22:50 -0300

    Subindo a segunda versão

--- api/src/main/java/com/geargames/api/models/Carrinho.java
+++ api/src/main/java/com/geargames/api/models/Carrinho.java
@@ -1,5 +1,21 @@
 package com.geargames.api.models;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
 public class Carrinho {
     
+    private String idUsuario;
+    private String itens;
+    private String preco;
+
 }
--- api/src/main/java/com/geargames/api/models/Jogos.java
+++ api/src/main/java/com/geargames/api/models/Jogos.java
@@ -1,7 +1,28 @@
 package com.geargames.api.models;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+
 public class Jogos {
     
+private String nome;
+private String descricao;
+private String preco;
+private String imagem;
+private String categoria;
+private String plataforma;
+private String requisitos;
+private String avaliacao;
 
-    
+
 }
